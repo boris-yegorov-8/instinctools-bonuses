76d6b07096445cab34b553562e0ca3f33890648e
boris-yegorov-8 <borisyegorov8@gmail.com>
2016-12-21 17:49:24 +0300

    Use Node.FS.Sync instead of FFI

--- src/Main.purs
+++ src/Main.purs
@@ -1,9 +1,16 @@
 module Main where
 
-import Prelude
+import Prelude (Unit)
+
+import Control.Bind ((=<<))
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (CONSOLE, log)
+import Control.Monad.Eff.Exception (EXCEPTION)
 
-main :: forall e. Eff (console :: CONSOLE | e) Unit
+import Node.Encoding (Encoding(..))
+import Node.FS (FS)
+import Node.FS.Sync (readTextFile)
+
+main :: forall e. Eff (console :: CONSOLE, err :: EXCEPTION, fs :: FS | e) Unit
 main = do
-  log "Hello sailor!"
+  log =<< readTextFile UTF8 "./credentials/credentials.json"
