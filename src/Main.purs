d977c6e08c95850364654b60515de6a4acdc3c36
boris-yegorov-8 <borisyegorov8@gmail.com>
2016-12-21 19:25:47 +0300

    WIP Parsing credentials

--- src/Main.purs
+++ src/Main.purs
@@ -1,16 +1,46 @@
 module Main where
 
-import Prelude (Unit)
+import Prelude (Unit, ($))
 
 import Control.Bind ((=<<))
 import Control.Monad.Eff (Eff)
-import Control.Monad.Eff.Console (CONSOLE, log)
+import Control.Monad.Eff.Console (CONSOLE, logShow)
 import Control.Monad.Eff.Exception (EXCEPTION)
+import Control.Monad.Except (runExcept)
 
 import Node.Encoding (Encoding(..))
 import Node.FS (FS)
 import Node.FS.Sync (readTextFile)
 
+import Data.Foreign (F)
+import Data.Foreign.Class (class IsForeign, readJSON, readProp)
+import Data.Foreign.Index (prop)
+
+data Foo = Foo Bar Baz
+
+data Bar = Bar String
+
+data Baz = Baz Number
+
+instance showFoo :: Show Foo where
+  show (Foo bar baz) = "(Foo " <> show bar <> " " <> show baz <> ")"
+
+instance showBar :: Show Bar where
+  show (Bar s) = "(Bar " <> show s <> ")"
+
+instance showBaz :: Show Baz where
+  show (Baz n) = "(Baz " <> show n <> ")"
+
+instance fooIsForeign :: IsForeign Foo where
+  read value = do
+    s <- value # (prop "foo" >=> readProp "bar")
+    n <- value # (prop "foo" >=> readProp "baz")
+    pure $ Foo (Bar s) (Baz n)
+
 main :: forall e. Eff (console :: CONSOLE, err :: EXCEPTION, fs :: FS | e) Unit
 main = do
-  log =<< readTextFile UTF8 "./credentials/credentials.json"
+  -- log =<< readTextFile UTF8 "./credentials/client_secret.json"
+  logShow $ runExcept $ readJSON """{ "x": 1, "y": 2 }""" :: F {
+      x :: Number
+      y :: Number
+    }
