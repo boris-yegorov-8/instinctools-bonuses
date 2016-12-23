9caefc2d9e2bfcc76ae6a80a454921cd41e15524
boris-yegorov-8 <borisyegorov8@gmail.com>
2016-12-23 17:57:50 +0300

    Simplify parsing credentials

--- src/Main.purs
+++ src/Main.purs
@@ -4,33 +4,33 @@ import Data.Show (class Show, show)
 import Data.Unit (Unit)
 import Data.Function (($), (#))
 import Data.Semigroup ((<>))
+import Data.Array (head)
+import Data.Maybe (fromMaybe)
 import Control.Applicative (pure)
 import Control.Bind (bind, (>=>))
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (CONSOLE, logShow)
 import Control.Monad.Eff.Exception (EXCEPTION)
 import Control.Monad.Except (runExcept)
-
 import Node.Encoding (Encoding(..))
 import Node.FS (FS)
 import Node.FS.Sync (readTextFile)
-
 import Data.Foreign (F)
 import Data.Foreign.Class (class IsForeign, readJSON, readProp)
 import Data.Foreign.Index (prop)
 
-data Credentials = Credentials String String (Array String)
+data Credentials = Credentials String String String
 
 instance showFoo :: Show Credentials where
-  show (Credentials clientId clientSecret redirectUris) =
-    "(Credentials " <> show clientId <> " " <> show clientSecret <> " " <> show redirectUris <> ")"
+  show (Credentials clientId clientSecret redirectUri) =
+    "(Credentials " <> show clientId <> " " <> show clientSecret <> " " <> show redirectUri <> ")"
 
 instance fooIsForeign :: IsForeign Credentials where
   read value = do
     clientId <- value # (prop "installed" >=> readProp "client_id")
     clientSecret <- value # (prop "installed" >=> readProp "client_secret")
     redirectUris <- value # (prop "installed" >=> readProp "redirect_uris")
-    pure $ Credentials clientId clientSecret redirectUris
+    pure $ Credentials clientId clientSecret $ fromMaybe "http://localhost" $ head redirectUris
 
 main :: forall e. Eff (console :: CONSOLE, err :: EXCEPTION, fs :: FS | e) Unit
 main = do
