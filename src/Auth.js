faf134a5a033d2d7d4aa595827cc02017acd7452
boris-yegorov-8 <borisyegorov8@gmail.com>
2016-12-26 20:46:55 +0300

    Implement JS wrapper for "google-auth-library" package

--- src/Auth.js
+++ src/Auth.js
@@ -1,3 +1,11 @@
-exports.createClient = function(token) {
-  return { a: 42, b: 73 };
+var googleAuth = require('google-auth-library');
+
+exports.createClient = function(options) {
+  var auth = new googleAuth();
+
+  var oauth2Client = new auth.OAuth2(options.clientId, options.clientSecret, options.redirectUrl);
+
+  oauth2Client.credentials = options.token;
+
+  return oauth2Client;
 };
