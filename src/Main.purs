675ab263925a66e9df66f28cca25ee03f99966b4
boris-yegorov-8 <borisyegorov8@gmail.com>
2016-12-30 18:24:50 +0300

    Read a file asynchronously

--- src/Main.purs
+++ src/Main.purs
@@ -9,12 +9,14 @@ import Control.Monad.Eff.Exception (EXCEPTION)
 import Control.Monad.Except (runExcept)
 import Node.Encoding (Encoding(..))
 import Node.FS (FS)
-import Node.FS.Sync (readTextFile)
+import Node.FS.Aff (readTextFile)
 import Data.Foreign (F, ForeignError)
 import Data.Foreign.Class (readJSON)
 import Data.Either (Either(..))
 import Data.Tuple (Tuple(..))
 import Data.List.NonEmpty (NonEmptyList)
+-- import Control.Semigroupoid ((<<<))
+import Control.Monad.Aff (Aff, runAff)
 
 import Credentials.ClientSecret (ClientSecret(..))
 import Credentials.Token (Token(..))
@@ -24,8 +26,7 @@ import Gmail (GmailEff, users)
 type EitherClientSecret = Either (NonEmptyList ForeignError) ClientSecret
 type EitherToken = Either (NonEmptyList ForeignError) Token
 
-readTextFileUtf8 :: forall t.
-  String -> Eff (fs :: FS, err :: EXCEPTION | t) String
+readTextFileUtf8 :: forall eff. String -> Aff (fs :: FS | eff) String
 readTextFileUtf8 = readTextFile UTF8
 
 credentialsFromJson :: String -> String -> Tuple EitherClientSecret EitherToken
@@ -41,13 +42,16 @@ credentialsToAuthOptions (ClientSecret id secret uri) (Token t) = {
   token: t
 }
 
-main :: forall e. Eff (users :: GmailEff, console :: CONSOLE, err :: EXCEPTION, fs :: FS | e) Unit
+-- main :: forall e. Eff (users :: GmailEff, console :: CONSOLE, err :: EXCEPTION, fs :: FS | e) Unit
 main = do
-  clientSecretContent <- readTextFileUtf8 "./credentials/client_secret.json"
-  tokenContent <- readTextFileUtf8 "./credentials/credentials.json"
-  case credentialsFromJson clientSecretContent tokenContent of
-    Tuple (Right clientSecret) (Right token) ->
-      users
-        (createClient $ credentialsToAuthOptions clientSecret token)
-        logShow
-    _ -> log "Wrong credentials"
+  runAff
+    logShow
+    logShow
+    (readTextFileUtf8 "./credentials/client_secret.json")
+  -- tokenContent <- readTextFileUtf8 "./credentials/credentials.json"
+  -- case credentialsFromJson clientSecretContent tokenContent of
+  --   Tuple (Right clientSecret) (Right token) ->
+  --     users
+  --       (createClient $ credentialsToAuthOptions clientSecret token)
+  --       logShow
+  --   _ -> log "Wrong credentials"
