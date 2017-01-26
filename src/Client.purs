f50db00e788fa034fd566633074dcac1ad63d148
boris-yegorov-8 <borisyegorov8@gmail.com>
2017-01-26 12:09:48 +0300

    Implement Client.getClient

--- src/Client.purs
+++ src/Client.purs
@@ -1,6 +1,6 @@
 module Client (getClient) where
 
-import Control.Monad.Aff (attempt)
+import Control.Monad.Aff (Aff(..), attempt)
 import Control.Monad.Eff.Console (logShow)
 import Control.Semigroupoid ((<<<))
 import Node.Encoding (Encoding(..))
@@ -15,15 +15,15 @@ import Control.Monad.Eff.Class (liftEff)
 import Data.Semigroup ((<>))
 import Data.Show (show)
 import Control.Monad.Eff.Exception (throwException, error)
+import Control.Applicative (pure)
 
 import Credentials.ClientSecret (ClientSecret(..))
 import Auth as Auth
 import Util (throwWrappedError)
 
-getClient = attempt <<< readTextFile UTF8 >=> liftEff <<< either
-  (throwWrappedError "Loading client secret file failed: ")
-  (\clientSecretContent -> either
-    (\_ -> throwException $ error "Parsing client secret JSON failed: ")
-    logShow
-    -- (\(ClientSecret c) -> Auth.createClient c)
-    (runExcept $ readJSON clientSecretContent :: F ClientSecret))
+getClient = attempt <<< readTextFile UTF8 >=> either
+  (liftEff <<< throwWrappedError "Loading client secret file failed: ")
+  (\content -> either
+    (\_ -> liftEff $ throwException $ error "Parsing client secret JSON failed: ")
+    (\(ClientSecret c) -> pure $ Auth.createClient c)
+    (runExcept $ readJSON content :: F ClientSecret))
