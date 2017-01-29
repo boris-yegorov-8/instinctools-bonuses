27dc5f7a49b4f30614d11ab88c91394b72c1d75a
boris <barys.yahorau@instinctools.ru>
2017-01-29 22:23:55 +0300

    Cleanup

--- src/Token.purs
+++ src/Token.purs
@@ -1,10 +1,9 @@
-module Token (getToken, refreshToken) where
+module Token (getToken) where
 
 import Control.Applicative (pure)
-import Control.Bind ((>>=), (>=>))
+import Control.Bind ((>>=))
 import Control.Monad.Aff (Aff, attempt, forkAff)
-import Control.Monad.Eff.Class (liftEff)
-import Control.Monad.Eff.Console (CONSOLE, log, logShow)
+import Control.Monad.Eff.Console (CONSOLE)
 import Control.Monad.Except (runExcept)
 import Data.Either (Either(..), either)
 import Data.Foreign (F)
@@ -14,6 +13,7 @@ import Node.Encoding (Encoding(..))
 import Node.FS (FS)
 import Node.FS.Aff (readTextFile, writeTextFile)
 import Data.Semigroup ((<>))
+import Node.ReadLine (READLINE)
 import Node.ReadLine.Aff.Simple (
   prompt,
   setPrompt,
@@ -24,12 +24,23 @@ import Node.ReadLine.Aff.Simple (
 import Control.Semigroupoid ((<<<))
 import Control.Apply ((*>))
 import Data.Show (show)
+import Control.Monad.Eff.Exception (EXCEPTION)
 
 import Auth as Auth
 import Credentials.Token (Token)
 import Constants (tokenPath, tokenOptions)
 import Util (throwWrappedError, throwError)
 
+refreshToken :: forall e.
+  Auth.Oauth2Client -> Aff
+    ( console :: CONSOLE
+    , readline :: READLINE
+    , getToken :: Auth.AuthEff
+    , err :: EXCEPTION
+    , fs :: FS
+    | e
+    )
+    Token
 refreshToken client =
   simpleInterface >>=
   (\interface ->
@@ -41,16 +52,28 @@ refreshToken client =
   (attempt <<< Auth.getToken client) >>=
   (either
     (throwWrappedError "Getting new token failed: ")
-    (\tokenJson ->
-      (forkAff $ writeTextFile UTF8 tokenPath $ show tokenJson) >>=
-      (\_ -> either
-        (\_ -> throwError "Wrong new token")
-        pure
-        (runExcept $ readJSON (show tokenJson) :: F Token))))
+    (pure <<< show)
+  ) >>=
+  (\tokenString ->
+    (forkAff $ writeTextFile UTF8 tokenPath tokenString) *>
+    (either
+      (\_ -> throwError "Wrong new token")
+      pure
+      (runExcept $ readJSON tokenString :: F Token)))
   where
     promptMessage = "Authorize this app by visiting this url: " <>
       Auth.generateAuthUrl client tokenOptions
 
+getToken :: forall e.
+  Auth.Oauth2Client -> Aff
+    ( console :: CONSOLE
+    , readline :: READLINE
+    , getToken :: Auth.AuthEff
+    , err :: EXCEPTION
+    , fs :: FS
+    | e
+    )
+    Token
 getToken client =
   attempt (
     (readTextFile UTF8 tokenPath) >>=
