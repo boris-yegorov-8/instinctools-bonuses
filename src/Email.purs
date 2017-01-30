3f3658e5538b419704adcb6de0b64c55066d43b5
boris-yegorov-8 <borisyegorov8@gmail.com>
2017-01-30 21:01:07 +0300

    WIP Move getting the email message from Main to Email

--- src/Email.purs
+++ src/Email.purs
@@ -1,1 +1,16 @@
 module Email (getMessage) where
+
+import Control.Monad.Aff (attempt)
+import Data.Function (($))
+
+import Auth as Auth
+
+getMessage client = attempt $ Auth.getMessages gmailOptions
+  where gmailOptions = {
+      auth: client,
+      userId: "me",
+      q: "subject:Позиции"
+    }
+
+-- showMessageIds "" messages = log $ show $ (\message -> message.id) <$> messages
+-- showMessageIds err _ = log $ "Gmail API failed: " <> err
--- src/Main.purs
+++ src/Main.purs
@@ -31,98 +31,14 @@ import Control.Applicative (pure)
 
 import Client (getClient)
 import Token (getToken)
+import Email (getMessage)
 
--- type EitherClientSecret = Either (NonEmptyList ForeignError) ClientSecret
--- type EitherToken = Either (NonEmptyList ForeignError) Token
-
--- then' :: forall m a b. (Bind m) => m a -> m b -> m b
--- then' ma mb = ma >>= (\_ -> mb)
---
--- infixl 1 then' as >>
-
--- showMessageIds :: forall t.
---     String
---  -> Array {id :: String}
---  -> Eff (console :: CONSOLE | t) Unit
--- showMessageIds "" messages = log $ show $ (\message -> message.id) <$> messages
--- showMessageIds err _ = log $ "Gmail API failed: " <> err
---
--- onLocalCredentialsRead :: forall e.
---   String -> String -> Eff
---     (console :: CONSOLE, getMessages :: GmailEff | e)
---     Unit
--- onLocalCredentialsRead clientSecretContent tokenContent = either
---   (logError "Wrong credentials: ")
---   (\(ClientSecret clientSecretObject) -> either
--- -- TODO: get new token
---     (logError "Authorize this app by visiting this url: ")
---     (\(Token tokenObject) ->
---       let
---         clientWithoutToken = Auth.createClient clientSecretObject
---         client = Auth.setToken tokenObject
---         gmailOptions = {
---           auth: client,
---           userId: "me",
---           q: "subject:Позиции"
---         }
---       in
---         getMessages gmailOptions showMessageIds)
---     (runExcept $ readJSON tokenContent :: F Token))
---   (runExcept $ readJSON clientSecretContent :: F ClientSecret)
---
--- onNewToken :: forall e.
---   String -> Json -> Eff (console :: CONSOLE, fs :: FS | e) Unit
--- onNewToken "" token =
---   (runAff
---     (logError "Getting new token failed: ")
---     (log <<< show)
---     (writeTextFile UTF8 Constants.tokenPath $ show token)) >> log "42"
--- onNewToken err _ = logError "Getting new token failed: " err
---
--- foo :: forall e.
---   String
---   -> Eff
---        ( console :: CONSOLE
---        , readline :: ReadLine.READLINE
---        , err :: EXCEPTION
---        , getToken :: Auth.AuthEff
---        , fs :: FS
---        | e
---        )
---        Unit
--- foo clientSecretContent = either
---   (logError "Wrong credentials: ")
---   (\(ClientSecret clientSecretObject) ->
---     let
---       oauth2client = Auth.createClient clientSecretObject
---       tokenOptions = {
---         access_type: "offline",
---         scope: "https://www.googleapis.com/auth/gmail.readonly"
---       }
---     in
---       log (
---         "Authorize this app by visiting this url: " <>
---         Auth.generateAuthUrl oauth2client tokenOptions
---       ) >>
---       (ReadLine.createConsoleInterface ReadLine.noCompletion) >>=
---       (\interface ->
---         (ReadLine.setPrompt "> " 2 interface) >>
---         (ReadLine.setLineHandler
---           interface
---           (\code -> ReadLine.close interface >>
---             Auth.getToken oauth2client code onNewToken)) >>
---         (ReadLine.prompt interface)))
---   (runExcept $ readJSON clientSecretContent :: F ClientSecret)
---
--- main = launchAff $ (readTextFileUtf8 Constants.clientSecretPath) >>=
---   either
---     (liftEff <<< logError "Loading client secret file failed: ")
---     (\clientSecretContent ->
---       (readTextFileUtf8 Constants.tokenPath) >>= liftEff <<< either
---         (\_ -> foo clientSecretContent)
---         (onLocalCredentialsRead clientSecretContent))
 main = runAff
   logShow
   logShow
   -- (\_ -> log "Successfully finished")
-  (getClient Constants.clientSecretPath >>= getToken)
+  (
+    (getClient Constants.clientSecretPath) >>=
+    getToken >>=
+    (\(Token t) -> getMessage $ Auth.setToken t)
+  )
