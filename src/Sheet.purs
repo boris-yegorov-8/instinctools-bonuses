48dba08b223cf38d0fb318f84c54c245a79c1683
boris-yegorov-8 <borisyegorov8@gmail.com>
2017-02-07 21:36:19 +0300

    WIP Calculate the data to update the sheet

--- src/Sheet.purs
+++ src/Sheet.purs
@@ -1,18 +1,19 @@
 module Sheet (Values(..), updateSheet) where
 
+import Constants (sheetId)
+import Control.Applicative (pure)
 import Control.Bind ((>>=), (>=>))
 import Control.Monad.Aff (Aff, attempt)
-import Data.Function (($))
+import Control.Monad.Except (runExcept)
+import Control.Semigroupoid ((<<<))
+import Data.Array (init)
 import Data.Either (either)
-import Data.Show (class Show, show)
-import Control.Applicative (pure)
 import Data.Foreign (F)
 import Data.Foreign.Class (class IsForeign, readProp, readJSON)
-import Control.Semigroupoid ((<<<))
-import Control.Monad.Except (runExcept)
-
+import Data.Function (($))
+import Data.Maybe (maybe, fromMaybe)
+import Data.Show (class Show, show)
 import GoogleSheets (getValues, batchUpdate)
-import Constants (sheetId)
 import Util (throwWrappedError, throwError)
 
 data Values = Values (Array (Array String))
@@ -23,14 +24,38 @@ instance showValues :: Show Values where
 instance valuesIsForeign :: IsForeign Values where
   read = (readProp "values") >=> (pure <<< Values)
 
+parseValues content = runExcept $ readJSON (show content) :: F Values
+
+foo message values mapping = fromMaybe [] $ init values
+
 updateSheet client message =
-  (attempt $ getValues { auth: client, spreadsheetId: sheetId, range: "A2:I" }) >>=
+  (attempt $ getValues $ options { range = "Sheet1!A2:I" }) >>=
   (either
     (throwWrappedError "Failed to get the spreadsheet: ")
     pure
   ) >>=
-  (\content -> either
-    (throwError "Failed to parse the spreadsheet")
-    pure
-    (runExcept $ readJSON (show content) :: F Values))
+  (
+    (either
+      (throwError
+        "Failed to parse the spreadsheet: ")
+      pure
+    ) <<< parseValues
+  ) >>=
+  (\(Values v) ->
+    (attempt $ getValues $ options { range = "Sheet2!A1:B" }) >>=
+    (either
+      (throwWrappedError
+        "Failed to get the mapping of the positions to the scores: ")
+      pure
+    ) >>=
+    (
+      (either
+        (throwError
+          "Failed to parse the mapping of the positions to the scores: ")
+        (pure <<< foo message v)
+      ) <<< parseValues
+    )
+  )
+  where
+    options = { auth: client, spreadsheetId: sheetId, range: "" }
   -- (\_ -> batchUpdate { auth: client, spreadsheetId: sheetId })
