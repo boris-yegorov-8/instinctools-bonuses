12169aab1fa4b0db99219d8e5577bdb5a5004516
boris-yegorov-8 <borisyegorov8@gmail.com>
2017-02-07 13:36:21 +0300

    WIP Update the sheet

--- src/GoogleSheets.js
+++ src/GoogleSheets.js
@@ -1,16 +1,83 @@
 var sheets = require('googleapis').sheets('v4');
 
 exports.getValues = function(options) {
-    return function(success, error) {
-        sheets.spreadsheets.values.get(options, function(err, response) {
-            if (err) {
-                error(err);
-            } else {
-                console.log('-------------');
-                console.log(response);
-                console.log('-------------');
-                success('42');
+  return function(success, error) {
+    sheets.spreadsheets.values.get(options, function(err, response) {
+      if (err) {
+        error(err);
+      } else {
+        success(response);
+      }
+    });
+  };
+};
+
+exports.batchUpdate = function(options) {
+  return function(success, error) {
+    // TODO: move request to purs
+    var request = {
+      auth: options.auth,
+      spreadsheetId: options.spreadsheetId,
+      resource: {
+        requests: [
+          // {
+          //   updateCells: {
+          //     start: { sheetId: 0, rowIndex: 156, columnIndex: 0 },
+          //     rows: [
+          //       {
+          //         values: [
+          //           {
+          //             userEnteredValue: { numberValue: 1 },
+          //           }
+          //         ]
+          //       }
+          //     ],
+          //     fields: 'userEnteredValue'
+          //   }
+          // },
+          // {
+          //   copyPaste: {
+          //     source: {
+          //       sheetId: 0,
+          //       startRowIndex: 152,
+          //       endRowIndex: 153,
+          //       startColumnIndex: 7,
+          //       endColumnIndex: 8
+          //     },
+          //     destination: {
+          //       sheetId: 0,
+          //       startRowIndex: 153,
+          //       endRowIndex: 154,
+          //       startColumnIndex: 7,
+          //       endColumnIndex: 8
+          //     },
+          //     pasteType: 'PASTE_FORMULA'
+          //   }
+          // },
+          {
+            insertDimension: {
+              range: {
+                sheetId: 0,
+                dimension: 'ROWS',
+                startIndex: 154,
+                endIndex: 160,
+              },
+              inheritFromBefore: true,
             }
-        });
+          }
+        ]
+      }
     };
+
+    sheets.spreadsheets.batchUpdate(request, function(err, response) {
+      if (err) {
+        error(err);
+      } else {
+        console.log('-------------');
+        console.log(response);
+        console.log('-------------');
+        success('42');
+      }
+    });
+  };
 };
--- src/GoogleSheets.purs
+++ src/GoogleSheets.purs
@@ -1,9 +1,11 @@
 module GoogleSheets (
   GoogleSheetsEff,
-  getValues
+  getValues,
+  batchUpdate
 ) where
 
 import Control.Monad.Aff (Aff)
+import Data.Argonaut.Core (Json)
 
 import Auth (Oauth2Client)
 
@@ -11,4 +13,8 @@ foreign import data GoogleSheetsEff :: !
 
 foreign import getValues :: forall e.
   { auth :: Oauth2Client, spreadsheetId :: String, range :: String } ->
+  Aff (getValues :: GoogleSheetsEff | e) Json
+
+foreign import batchUpdate :: forall e.
+  { auth :: Oauth2Client, spreadsheetId :: String } ->
   Aff (getValues :: GoogleSheetsEff | e) String
