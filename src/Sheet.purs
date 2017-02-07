97cd454d7cdd07db94357049d83c7191a35a1bbb
boris-yegorov-8 <borisyegorov8@gmail.com>
2017-02-07 13:37:06 +0300

    Parse the sheet

--- src/Sheet.purs
+++ src/Sheet.purs
@@ -1,13 +1,36 @@
-module Sheet (updateSheet) where
+module Sheet (Values(..), updateSheet) where
 
-import GoogleSheets (getValues)
+import Control.Bind ((>>=), (>=>))
+import Control.Monad.Aff (Aff, attempt)
+import Data.Function (($))
+import Data.Either (either)
+import Data.Show (class Show, show)
+import Control.Applicative (pure)
+import Data.Foreign (F)
+import Data.Foreign.Class (class IsForeign, readProp, readJSON)
+import Control.Semigroupoid ((<<<))
+import Control.Monad.Except (runExcept)
+
+import GoogleSheets (getValues, batchUpdate)
 import Constants (sheetId)
+import Util (throwWrappedError, throwError)
 
-updateSheet client message = getValues options
-  where
-    options =
-      {
-        auth: client,
-        spreadsheetId: sheetId,
-        range: "A2:I155"
-      }
+data Values = Values (Array (Array String))
+
+instance showValues :: Show Values where
+  show (Values v) = show v
+
+instance valuesIsForeign :: IsForeign Values where
+  read = (readProp "values") >=> (pure <<< Values)
+
+updateSheet client message =
+  (attempt $ getValues { auth: client, spreadsheetId: sheetId, range: "A2:I" }) >>=
+  (either
+    (throwWrappedError "Failed to get the spreadsheet: ")
+    pure
+  ) >>=
+  (\content -> either
+    (throwError "Failed to parse the spreadsheet")
+    pure
+    (runExcept $ readJSON (show content) :: F Values))
+  -- (\_ -> batchUpdate { auth: client, spreadsheetId: sheetId })
