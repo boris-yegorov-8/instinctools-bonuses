be20e56b65c019cbe32e590f3edc80a6777b438d
boris-yegorov-8 <borisyegorov8@gmail.com>
2017-02-18 14:55:15 +0300

    Add type signature for main

--- src/Main.purs
+++ src/Main.purs
@@ -1,18 +1,49 @@
 module Main where
 
 import Control.Bind ((>>=))
-import Control.Monad.Aff (runAff)
-import Control.Monad.Eff.Console (logShow, log)
+import Control.Monad.Aff (Canceler, runAff)
+import Control.Monad.Eff.Console (CONSOLE, logShow, log)
 import Data.Function (($))
+import Control.Monad.Eff (Eff)
+import Node.FS (FS)
+import Control.Monad.Eff.Exception (EXCEPTION)
+import Node.ReadLine (READLINE)
+import Node.Buffer (BUFFER)
 
 import Credentials.Token (Token(..))
 import Client (getClient)
 import Token (getToken)
 import Email (getMessage)
-import Auth as Auth
+import Auth (AuthEff, setToken)
 import Constants as Constants
 import Sheet (updateSheet)
+import Google.Gmail (GmailEff)
+import Google.Sheets (GoogleSheetsEff)
 
+main :: forall e.
+  Eff
+    ( console :: CONSOLE
+    , fs :: FS
+    , err :: EXCEPTION
+    , readline :: READLINE
+    , getToken :: AuthEff
+    , getMessages :: GmailEff
+    , buffer :: BUFFER
+    , getValues :: GoogleSheetsEff
+    | e
+    )
+    (Canceler
+       ( console :: CONSOLE
+       , fs :: FS
+       , err :: EXCEPTION
+       , readline :: READLINE
+       , getToken :: AuthEff
+       , getMessages :: GmailEff
+       , buffer :: BUFFER
+       , getValues :: GoogleSheetsEff
+       | e
+       )
+    )
 main = runAff
   logShow
   (\_ -> log "Successfully finished")
@@ -20,7 +51,7 @@ main = runAff
     (getClient Constants.clientSecretPath) >>=
     (\client ->
       (getToken client) >>=
-      (\(Token t) -> getMessage $ Auth.setToken t) >>=
+      (\(Token t) -> getMessage $ setToken t) >>=
       (updateSheet client)
     )
   )
